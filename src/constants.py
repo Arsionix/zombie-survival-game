a124d1a2f295b7620534890a99720299ea59288e
Arseniy <arsenii.tyapkin@vk.com>
2026-01-06 15:43:14 +0700

    feat(project): clean project setup - only initialization (#2)

--- src/constants.py
+++ src/constants.py
@@ -1,26 +1,9 @@
-"""
-Константы игры
-"""
+"""Константы и настройки игры"""
 
 import arcade
 
-# Окно
-SCREEN_WIDTH = 1000
-SCREEN_HEIGHT = 700
+SCREEN_WIDTH = 1024
+SCREEN_HEIGHT = 768
 SCREEN_TITLE = "Zombie Survival"
 
-# Игрок
-PLAYER_SPEED = 5
-PLAYER_START_X = SCREEN_WIDTH // 2
-PLAYER_START_Y = SCREEN_HEIGHT // 2
-PLAYER_SIZE = 40
-
-# Состояния анимации
-PLAYER_IDLE = 0
-PLAYER_MOVING = 1
-
-# Цвета
-BACKGROUND_COLOR = arcade.color.DARK_SLATE_GRAY
-PLAYER_COLOR = arcade.color.BLUE
-PLAYER_NOSE_COLOR = arcade.color.YELLOW
-UI_COLOR = arcade.color.WHITE
+BACKGROUND_COLOR = arcade.color.ASH_GREY
--- src/game.py
+++ src/game.py
@@ -1,69 +1,17 @@
-"""
-Основной класс игры - связывает всё вместе
-"""
+"""Основной класс игры"""
 
 import arcade
 from .constants import *
-from .player import Player
-from .bullet import Bullet
 
 
 class GameWindow(arcade.Window):
-    """Главное окно игры"""
-
-    def __init__(self):
-        super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
-
-        self.player = None
-        self.bullets = []
-
-        self.mouse_x = SCREEN_WIDTH // 2
-        self.mouse_y = SCREEN_HEIGHT // 2
+    def __init__(self, width=SCREEN_WIDTH, height=SCREEN_HEIGHT, title=SCREEN_TITLE):
+        super().__init__(width, height, title)
+        arcade.set_background_color(BACKGROUND_COLOR)
 
     def setup(self):
-        """Настройка начального состояния игры"""
-        self.player = Player()
-        self.bullets = []
-
-        self.set_mouse_visible(True)
+        """Настройка игры"""
 
     def on_draw(self):
+        """Отрисовка"""
         self.clear()
-
-        self.player.draw()
-
-        for bullet in self.bullets:
-            bullet.draw()
-
-    def on_update(self, delta_time):
-        """Обновление игровой логики"""
-        self.player.update(delta_time)
-
-        self.player.face_mouse(self.mouse_x, self.mouse_y)
-
-        bullets_to_remove = []
-        for bullet in self.bullets:
-            bullet.update()
-            if bullet.is_off_screen():
-                bullets_to_remove.append(bullet)
-
-        for bullet in bullets_to_remove:
-            self.bullets.remove(bullet)
-
-    def on_key_press(self, key, modifiers):
-        """Обработка нажатия клавиш"""
-        self.player.on_key_press(key)
-
-    def on_key_release(self, key, modifiers):
-        """Обработка отпускания клавиш"""
-        self.player.on_key_release(key)
-
-    def on_mouse_motion(self, x, y, dx, dy):
-        """Движение мыши - для поворота игрока"""
-        self.mouse_x = x
-        self.mouse_y = y
-
-    def on_mouse_press(self, x, y, button, modifiers):
-        """Нажатие мыши - стрельба (завтра)"""
-        if button == arcade.MOUSE_BUTTON_LEFT:
-            pass
